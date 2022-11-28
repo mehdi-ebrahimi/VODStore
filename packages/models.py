c68ffc152e070c22814d0a2bc791b48f09858198
mehdi-ebrahimi <emehdi@live.com>
2022-11-28 18:26:14 +0330

    Git initialize and gitignore add ....

--- packages/models.py
+++ packages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Package(models.Model):
+    # parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
+    # id_user = models.ForeignKey('User', verbose_name=_('user'), on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='packages/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        # db_table = 'categories'
+        verbose_name = _('Package')
+        verbose_name_plural = _('Packages')
+
+    def __str__(self):
+        return self.title
