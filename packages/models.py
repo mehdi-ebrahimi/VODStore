58dfdd0c8b5bdfe27d29cd81883d45558e00237b
mehdi-ebrahimi <emehdi@live.com>
2022-11-28 19:23:19 +0330

    packages added...

--- packages/models.py
+++ packages/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Package(models.Model):
-    # parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
-    # id_user = models.ForeignKey('User', verbose_name=_('user'), on_delete=models.CASCADE)
+    # user = models.ForeignKey('User', verbose_name=_('user'), on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='packages/')
@@ -14,8 +13,35 @@ class Package(models.Model):
 
     class Meta:
         # db_table = 'categories'
-        verbose_name = _('Package')
-        verbose_name_plural = _('Packages')
+        verbose_name = _('package')
+        verbose_name_plural = _('packages')
+
+    def __str__(self):
+        return self.title
+    
+
+class File(models.Model):
+    FILE_AUDIO = 1
+    FILE_VIDEO = 2
+    FILE_PDF = 3
+    FILE_TYPES = (
+        (FILE_AUDIO, _('audio')),
+        (FILE_VIDEO, _('video')),
+        (FILE_PDF, _('pdf'))
+    )
+
+    package = models.ForeignKey('package', verbose_name=_('package'), related_name='files', on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    file_type = models.PositiveSmallIntegerField(_('file type'), choices=FILE_TYPES)
+    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        # db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
 
     def __str__(self):
         return self.title
